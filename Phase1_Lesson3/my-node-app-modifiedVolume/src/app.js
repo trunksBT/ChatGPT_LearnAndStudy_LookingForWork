619980cde3422fa9f731e7abb3a1e156b8c6baef
trunksBT <korytko07@gmail.com>
2025-08-06 00:00:20 +0200

    Works code run on the dockering Ndoe.js

--- Phase1_Lesson3/my-node-app-modifiedVolume/src/app.js
+++ Phase1_Lesson3/my-node-app-modifiedVolume/src/app.js
@@ -1,1 +1,20 @@
 console.log("Hello modified 2 from inside a Docker container!");
+
+const { MongoClient } = require('mongodb');
+
+const url = "mongodb://mongo:27017";
+const dbName = "testdb";
+
+(async function () {
+    try{
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection("messages")
+        await collection.insertOne({ msg: "Hello from Dockerized Node.js!" });
+        const docs = await collection.find().toArray();
+        console.log("Messages from DB:", docs);
+        client.close();
+    } catch (err){
+        console.error("DB connection error:", err);
+    }
+})();
